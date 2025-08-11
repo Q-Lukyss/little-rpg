6dbd166c16d2cdec13828bdeae00ba704400c841
Q-Lukyss <quentin.lkss@gmail.com>
2025-08-11 23:21:43 +0200

    creation struct qui reprensente les stats je pense que c est mieux que l enum a terme

--- src/player/stats.rs
+++ src/player/stats.rs
@@ -2,6 +2,20 @@
 pub enum Stat {
     Attack,
     Hp,
+    MaxHp,
     Crit,
-    Defense
+    Defense,
+    Level
 }
+
+// a terme remplacera l enum je pense
+pub struct Stats {
+    attack : i32,
+    hp : i32,
+    maxHp : i32,
+    crit : f32,
+    defense : i32,
+    xp : i32,
+    xp_to_level_up : i32,
+    level : u8, // max 255 suffiant
+} 
